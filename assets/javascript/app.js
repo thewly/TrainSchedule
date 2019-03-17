bb39ee7def9789442c73f4abffbbb8b64644a4e9
Matthew Cole <colematthewg@gmail.com>
2019-03-17 15:03:25 -0500

    functioning app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,21 +12,45 @@
   
   var database = firebase.database();
   var employeeList = database.ref('/employee');
-
+  
+  var name = "";
+  var destination = "";
+  var frequency = "";
+  var firstTrain = "";
+  
+  
   employeeList.on('child_added', function(snap){
+    
+    // var formattedDate = moment.unix(record.start).format('MM/DD/YYYY');
+    var nextArrival = 0;
+    var minutesAway = 0;
+    var firstTrain = snap.val().firstTrain;
+    var frequency = snap.val().frequency;
+    var currentTime = moment().format('HH:MM');
+    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-        var formattedDate = moment.unix(record.start).format('MM/DD/YYYY');
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
 
-      $('tbody').append(`
-          <tr>
-              <td>${snap.val().name}</td>
-              <td>${snap.val().role}</td>
-              <td>${snap.val().email}</td>
-              <td>${snap.val().rate}</td>
-              <td>${snap.val().startDate}</td>
-              <td>${snap.val()}
-          </tr>
-        `)
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:MM"));
+
+    $('tbody').append(`
+    <tr>
+    <td>${snap.val().name}</td>
+    <td>${snap.val().destination}</td>
+    <td>${snap.val().frequency}</td>
+    <td>${nextTrain}</td>
+    <td>${tMinutesTillTrain}</td>
+    </tr>
+    `)
   })
   
   $("#submitButton").on("click", function() {
@@ -35,26 +59,18 @@
       var newRow = $("<div>");
       newRow.addClass("doesThisWork");
       newRow.append("<div class='row'>");
-      
-      var name = "";
-      var role = "";
-      var email = "";
-      var rate = "";
-      var startDate = "";
 
     
-      name = $("#employeeName").val().trim();
-      role = $("#employeeRole").val().trim();
-      email = $("#employeeEmail").val().trim();
-      rate = $("#employeeRate").val().trim();
-      startDate = $("#employeeStart").val().trim();
+      name = $("#trainName").val().trim();
+      destination = $("#destination").val().trim();
+      frequency = $("#frequency").val().trim();
+      firstTrain = $("#firstTrain").val().trim();
 
       database.ref("/employee").push({
         name: name,
-        role: role,
-        email: email,
-        rate: rate,
-        startDate: startDate
+        destination: destination,
+        frequency: frequency,
+        firstTrain: firstTrain
 
       })
     });
